ddad5d8c830d5b597239139ae4ff38d6cdc0bc4e
Johan Modin <carljohan.modin@gmail.com>
2025-04-16 15:29:33 +0200

    Add assignment 2 finished

--- Course 4(Development in Embedded Systems)/C++/Workshop 4/Uppgift2/src/main.cpp
+++ Course 4(Development in Embedded Systems)/C++/Workshop 4/Uppgift2/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int temperature = 10;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+LiquidCrystal_I2C lcd1(0x20, 16, 2);
+LiquidCrystal_I2C lcd2(0x26, 16, 2);
+LiquidCrystal_I2C lcd3(0x23, 16, 2);
+
+void setup(){  
+  lcd1.init();
+  lcd1.backlight();
+  lcd2.init();
+  lcd2.backlight();
+  lcd3.init();
+  lcd3.backlight();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  
+  lcd3.clear();
+  lcd2.clear();
+  lcd1.clear();
+  lcd3.print("C: " + String(calcCelsius(temperature), 2) + " degrees");
+  lcd2.print("F: " + String(calcFahrenheit(temperature), 2) + " degrees");
+  lcd1.print("K: " + String(calcKelvin(temperature), 2) + " degrees");
+  
+  temperature ++;
+  delay (2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+float calcKelvin (int temp){
+  return temp + 273.15;
+}
+
+float calcFahrenheit(int temp){
+  return(temp * 9.0 / 5.0) + 32; 
+}
+
+float calcCelsius(int temp){
+  return temp;
 }
