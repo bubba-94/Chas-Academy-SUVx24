70879a810280b7ec1c799aea0d889019b51a5569
Johan Modin <carljohan.modin@gmail.com>
2025-08-28 10:31:07 +0200

    Test commmit - First .yaml config with clang-format - Remember identation in and case sensitivity

---
+++ Course 5(Advanced System Development)/Workshop 03/AllmanStyleFormatting/src/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * ASSIGNMENT 1
+ *
+ * Purpose:
+ *
+ * Get prepared for using code formatting Allman-style
+ *
+ * Configure .clang format and integrate it into the workflow
+ *
+ * Get familiar with git pre-commit hooks and hur they are used to automize
+ * ensure a coding standard
+ *
+ * Exercise using the tools Git + pre-commit + .clang format to create a
+ * consistent and professional work environment
+ *
+ */
+
+#include <arduino.h>
+
+void setup()
+{
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop()
+{
+  // Blinkar varje sekund
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  delay(1000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(1000);
+}
